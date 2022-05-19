3f531d0f288a5063a0fd7c262ba4dc3289fe4a22
NathanF2001~ <nathanfreitas403@gmail.com>
2022-05-19 18:43:53 -0300

    Consertando bug de atualizar o banco

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let express = require("express");
 let path = require("path");
 let cookieParser = require("cookie-parser");
 let logger = require("morgan");
+const cors = require('cors');
 
 let usersRouter = require("./routes/users.router");
 
@@ -14,6 +15,7 @@ app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
